a5090067c4ebb93345b3254f9e93c64d59628e5a
fizz08c <152685950+fizz08c@users.noreply.github.com>
2024-08-12 23:14:13 -0700

    m012

--- tcp_server_new/Modbus.c
+++ tcp_server_new/Modbus.c
@@ -3,7 +3,7 @@
  File name:   Modbus.c
  Description: 
  Author:      Zhang Jiawei
- Version:     V011
+ Version:     V012
  Data:        2024-8
  *****************************************************************/
 #include <stdlib.h>
